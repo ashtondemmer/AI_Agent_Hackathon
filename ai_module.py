739c25a1d2194c8f7d1ee013f78045219f92a463
Ashton Demmer <a.demmer28@gmail.com>
2023-08-19 14:19:02 -0500

    changes

--- ai_module.py
+++ ai_module.py
@@ -1,7 +1,18 @@
 import api_module
 
+import os
+import cohere
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
 def run_ai(input):
     #Add AI Agent Here:
+    
+    #api that gives me images of dogs
+    
+    #dog pictures
+    #api that gives images of cats
+    
 
     #output = api_module.fetch(input_after_ai)
     output = input
