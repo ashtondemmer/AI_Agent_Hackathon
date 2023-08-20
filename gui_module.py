17843eb7098af55a512484bec0820f373526f48c
Ashton Demmer <a.demmer28@gmail.com>
2023-08-19 21:51:24 -0500

    gui

--- gui_module.py
+++ gui_module.py
@@ -12,31 +12,33 @@ class GUI:
                   "lightPurple":"#320064"}
         self.root = root
         self.root.title("QuickRead")
-        self.root.geometry("1100x550")
+        self.root.geometry("1100x650")
         self.root._set_appearance_mode("Dark")
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
 
         #Left Side
-        self.user_input_frame = ctk.CTkFrame(self.root, fg_color=self.colors["darkGray"])
-        self.user_input_frame.grid(row=0, column=1, sticky='nsew')
-        self.user_input_frame.grid_rowconfigure(1, weight=1)
-        self.header = ctk.CTkLabel(self.user_input_frame, text="QuickRead", font=('Open Sans', 26))
+        self.left_frame = ctk.CTkFrame(self.root, fg_color=self.colors["darkGray"])
+        self.left_frame.grid(row=0, column=0, sticky='nsew')
+        self.left_frame.grid_rowconfigure(1, weight=1)
+        self.header = ctk.CTkLabel(self.left_frame, text="QuickRead", font=('Open Sans', 26))
         self.header.grid(row=0, columnspan=2, column=0, pady=10)
-        self.sub_header = ctk.CTkLabel(self.user_input_frame, text="A summarizing app powered by artificial intelligence", font=('Open Sans', 18))
-        self.sub_header.grid(row=1, columnspan=2, column=0)
-        self.body_frame = ctk.CTkFrame(self.user_input_frame, fg_color=self.colors["darkGray"])
-        self.body_frame.grid(row=2, column=0, columnspan=2, sticky='nsew')
-        self.user_input_frame.grid_columnconfigure(0, weight=1)
-        self.pad_frame = ctk.CTkFrame(self.user_input_frame, bg_color=self.colors["darkGray"], fg_color=self.colors['darkGray'])
-        self.pad_frame.grid_columnconfigure(0, weight=1)
-        self.pad_frame.grid(row=3, column=0, sticky='ew', pady=15, padx=15)
-        self.entry = ctk.CTkEntry(self.pad_frame, placeholder_text="Tell us what API you're looking for...", height=50)
-        self.entry.grid(row=2, column=0, sticky='ew')
-        self.entry.bind("<Return>", self.input_to_output)
-        self.ask_button = ctk.CTkButton(self.pad_frame, command=self.input_to_output, text=">", width=60, height=50, fg_color=self.colors["lightPurple"], font=('Open Sans', 18))
-        self.ask_button.grid(row=2, column=1, sticky='ew')
+        self.summarized_text = ctk.CTkLabel(self.left_frame, text="Your summarized text will appear here.", font=('Open Sans', 14), wraplength=450)
+        self.summarized_text.grid(row=1, columnspan=2, column=0)
+        # self.body_frame = ctk.CTkFrame(self.left_frame, fg_color=self.colors["darkGray"])
+        # self.body_frame.grid(row=2, column=0, columnspan=2, sticky='nsew')
+        self.left_frame.grid_columnconfigure(0, weight=1)
+
+            #Old txt input and submit button
+        # self.pad_frame = ctk.CTkFrame(self.left_frame, bg_color=self.colors["darkGray"], fg_color=self.colors['darkGray'])
+        # self.pad_frame.grid_columnconfigure(0, weight=1)
+        # self.pad_frame.grid(row=3, column=0, sticky='ew', pady=15, padx=15)
+        # self.entry = ctk.CTkEntry(self.pad_frame, placeholder_text="Tell us what API you're looking for...", height=50)
+        # self.entry.grid(row=2, column=0, sticky='ew')
+        # self.entry.bind("<Return>", self.input_to_output)
+        # self.ask_button = ctk.CTkButton(self.pad_frame, command=self.input_to_output, text=">", width=60, height=50, fg_color=self.colors["lightPurple"], font=('Open Sans', 18))
+        # self.ask_button.grid(row=2, column=1, sticky='ew')
 
         self.suggestionTexts = [
             "Paste long emails that take too much time to read.",
@@ -44,8 +46,8 @@ class GUI:
             "Review topics from your school textbook before your next exam.",
             "Open files from your computer within QuickRead for easy summarization!"
         ]
-        self.suggestion_frame = ctk.CTkFrame(self.body_frame)
-        self.suggestion_frame.pack(side="bottom", pady=25)
+        self.suggestion_frame = ctk.CTkFrame(self.left_frame)
+        self.suggestion_frame.grid(row=2, column=0, pady=25)
         self.suggestion_header = ctk.CTkLabel(self.suggestion_frame, text="Some uses for QuickRead:")
         self.suggestion_header.grid(row=0, column=0, columnspan=2)
         for index, text in enumerate(self.suggestionTexts):
@@ -57,7 +59,7 @@ class GUI:
             label.pack(padx=5, pady=5)
 
         #Right Side
-        self.user_input_frame.grid(row=0, column=0)
+        self.left_frame.grid(row=0, column=0)
         self.right_frame = ctk.CTkFrame(self.root, fg_color=self.colors['lightGray'])
         self.right_frame.grid(row=0, column=1, sticky='nsew')
 
@@ -77,24 +79,29 @@ class GUI:
             sub_frame.grid(row=1, column=column, padx=5, pady=5, sticky='nsew')
             label = ctk.CTkLabel(sub_frame, text=text, font=("Arial", 14), height=50, width=20, wraplength=175)
             label.pack(padx=5, pady=5)
+
+        self.input_field = ctk.CTkTextbox(self.right_frame)
+        self.input_field.insert("1.0", "Paste what you want to be summarized here...")
+        self.input_field.pack(fill="both", expand=True, padx=15)
         
-        self.upload_file_button = ctk.CTkButton(self.right_frame, text="Upload File", command=self.upload_file)
-        self.upload_file_button.pack(side='bottom', pady=15)
-        self.btn_header = ctk.CTkLabel(self.right_frame, text="Upload a file to be summarized:")
-        self.btn_header.pack(side='bottom')
+        self.btn_frame = ctk.CTkFrame(self.right_frame)
+        self.btn_frame.pack(side='bottom', pady=15)
+        self.summarize_btn = ctk.CTkButton(self.btn_frame, text="Summarize", command=self.summarize)
+        self.summarize_btn.grid(row=0, column=0, padx=15)
+        self.upload_file_button = ctk.CTkButton(self.btn_frame, text="Upload File", command=self.upload_file)
+        self.upload_file_button.grid(row=0, column=1, padx=15)
 
 
-    def input_to_output(self, event=None):
-        input = self.entry.get()
-        self.entry.delete(0, ctk.END)
+
+    def summarize(self, event=None):
+        input = self.input_field.get("1.0", "end-1c")
         self.response = ai_module.run_ai(input)
-        self.response_label = ctk.CTkLabel(self.body_frame, text=self.response,  wraplength=500, bg_color=self.colors['lightPurple'], anchor="w")
-        self.response_label.pack()
+        self.summarized_text.configure(text=self.response)
 
     def upload_file(self, event=None):
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.file_label.config(text=f"Selected File: {file_path}")
+            self.file_label.configure(text=f"Selected File: {file_path}")
 
 
 
